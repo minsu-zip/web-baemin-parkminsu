f178be103bffca39d955c555db299a208f334223
minsu-zip <alstn4814@gmail.com>
2022-07-07 14:26:28 +0900

    feat : 회원가입 페이지 라우팅

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan')
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var signInRouter = require('./routes/signIn')
+var signUpRouter = require('./routes/signUp/index')
 
 var app = express()
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/signIn', signInRouter)
+app.use('/signUp', signUpRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
