d9b3e45282d97ef215df09b4881391f31c8f3f11
minsu-zip <alstn4814@gmail.com>
2022-07-10 23:52:54 +0900

    feat : 로그인 페이지 db연동

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+const db = require('../public/database/db.js')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,9 +8,15 @@ router.get('/', function (req, res, next) {
 })
 
 router.post('/', function (req, res, next) {
-  const [userId, password] = [req.body.userId, req.body.password]
-  req.session.userId = userId
-  res.redirect('/')
+  const { email, password } = req.body
+  const user = db.get('users').find({ email }).value()
+
+  if (user) {
+    req.session.nickname = user.nickname
+    res.redirect('/')
+  } else {
+    res.redirect('/signIn')
+  }
 })
 
 module.exports = router
