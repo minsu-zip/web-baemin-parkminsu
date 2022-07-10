5a811d55fda474c6c94fefd4fa77bf2572c0f34f
minsu-zip <alstn4814@gmail.com>
2022-07-10 14:14:47 +0900

    refactor : 정규표현식 로직 합치기

--- public/javascripts/signUpPhone.js
+++ public/javascripts/signUpPhone.js
@@ -2,8 +2,8 @@ const $phoneNumber = document.querySelector('.input-phoneNumber')
 
 // 휴대폰 번호 입력 검증
 const verifyPhoneNumber = (e) => {
-  e.target.value = e.target.value.replace(/[^0-9]/g, '')
   e.target.value = e.target.value
+    .replace(/[^0-9]/g, '')
     .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3')
     .replace(/\-{1,2}$/g, '')
 }
