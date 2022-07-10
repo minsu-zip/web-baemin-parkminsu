ffd446bec1703c5a7283c0be3d07999cb21aba6a
minsu-zip <alstn4814@gmail.com>
2022-07-10 23:57:32 +0900

    refactor : 불필요한 코드 제거

--- public/database/db.js
+++ public/database/db.js
@@ -1,14 +1,6 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('./public/database/db.json')
-
 const db = low(adapter)
-
-db.get('users').push({
-  email: 'mimsu@minsu.com',
-  nickname: 'minsu',
-  password: 'asdzxc123!@#',
-  birthday: '1999.01.01',
-})
 
 module.exports = db
