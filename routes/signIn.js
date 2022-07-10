ada32a37c8b4d5e1b3ac5a708e8820ef618dff64
minsu-zip <alstn4814@gmail.com>
2022-07-11 00:30:14 +0900

    feat : 로그인 성공 여부에 따른 세션 처리

--- routes/signIn.js
+++ routes/signIn.js
@@ -4,17 +4,19 @@ const db = require('../public/database/db.js')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('signIn')
+  const error = req.session.error || false
+  res.render('signIn', { error })
 })
 
 router.post('/', function (req, res, next) {
   const { email, password } = req.body
   const user = db.get('users').find({ email }).value()
-
-  if (user) {
+  if (user?.password === password) {
+    req.session.error = false
     req.session.nickname = user.nickname
     res.redirect('/')
   } else {
+    req.session.error = true
     res.redirect('/signIn')
   }
 })
